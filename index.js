4d7c1ab1ac014da1c2c1ffd5ad87939d555c8df9
Abraham vidal <antonio.vidal95@hotmail.com>
2020-07-13 17:22:27 -0400

    ajustes deployment

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 app.use(express.json({ extended: true }));
 
-const port = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 app.use("/public", express.static(`${__dirname}/storage/img`));
 
@@ -18,6 +18,6 @@ app.use("/api/proyectos", require("./routes/proyectos"));
 app.use("/api/usuario", require("./routes/usuario"));
 app.use("/api/auth", require("./routes/auth"));
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
